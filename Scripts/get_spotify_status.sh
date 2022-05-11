48676721dd585a02f1e696fbd9584f9ecbdcbc17
spleenftw <spleenftw@gmail.com>
2022-05-11 14:24:23 +0200

    update Mai 2022

--- Scripts/get_spotify_status.sh
+++ Scripts/get_spotify_status.sh
@@ -10,6 +10,8 @@ PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 # Use `playerctld` to always detect the latest player.
 # See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
 PLAYER="spotify"
+PLAYER="brave-browser"
+
 
 # Format of the information displayed
 # Eg. {{ artist }} - {{ album }} - {{ title }}
